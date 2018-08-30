88c26cffb92b92e85417ad72e2a040d178fa3f66
FernandoCagale <fernando.b.cagale@gmail.com>
2018-08-29 23:43:22 -0300

    add error

---
+++ error/error.go
@@ -0,0 +1,36 @@
+package error
+
+import (
+	"net/http"
+)
+
+//ResponseError struct
+type ResponseError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+//AddInternalServerError InternalServerError
+func AddInternalServerError(message string) ResponseError {
+	return ResponseError{http.StatusInternalServerError, message}
+}
+
+//AddNotFoundError NotFound
+func AddNotFoundError(message string) ResponseError {
+	return ResponseError{http.StatusNotFound, message}
+}
+
+//AddUnauthorizedError Unauthorized
+func AddUnauthorizedError(message string) ResponseError {
+	return ResponseError{http.StatusUnauthorized, message}
+}
+
+//AddBadRequestError BadRequest
+func AddBadRequestError(message string) ResponseError {
+	return ResponseError{http.StatusBadRequest, message}
+}
+
+//AddMethodNotAllowedError MethodNotAllowed
+func AddMethodNotAllowedError(message string) ResponseError {
+	return ResponseError{http.StatusMethodNotAllowed, message}
+}
